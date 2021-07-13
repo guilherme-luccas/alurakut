9b87f95765f79002c417e8ad74b656d43f1896e7
Guilherme Luccas <guilherme.luccas@outlook.com>
2021-07-13 12:18:07 -0300

    aula1

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,63 @@
-import styled from 'styled-components'
+import { MainGrid } from "../src/components/MainGrid";
+import { Box } from "../src/components/Box";
+import { ProfileRelationsBoxWrapper } from "../src/components/ProfileRelations";
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import {
+  AlurakutMenu,
+  OrkutNostalgicIconSet,
+} from "../src/lib/AlurakutCommons";
+
+function ProfileSideBar(props) {
+  return (
+    <Box>
+      <img
+        src={`https://github.com/${props.githubUser}.png`}
+        alt="profilepicture"
+        style={{ borderRadius: "0.41vw" }}
+      />
+    </Box>
+  );
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+  const githubUser = "guilherme-luccas";
+  const pessoasFavoritas = ["juunegreiros", "omariosouto", "rafaballerini"];
+  return (
+    <>
+      <AlurakutMenu />
+      <MainGrid>
+        <div className="profileArea" style={{ gridArea: "profileArea" }}>
+          <ProfileSideBar githubUser={githubUser} />
+        </div>
+        <div className="welcomeArea" style={{ gridArea: "welcomeArea" }}>
+          <Box>
+            <h1 className="title">Bem vindo</h1>
+            <OrkutNostalgicIconSet />
+          </Box>
+        </div>
+        <div
+          className="profileRelationsArea"
+          style={{ gridArea: "profileRelationsArea" }}
+        >
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">
+              Pessoas da comunidades ({pessoasFavoritas.length})
+            </h2>
+            <ul>
+              {pessoasFavoritas.map((pessoa) => {
+                return (
+                  <li>
+                    <a href={`/users/${pessoa}`} key={pessoa}>
+                      <img src={`https://github.com/${pessoa}.png`} />
+                      <span>{pessoa}</span>
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
+        </div>
+      </MainGrid>
+    </>
+  );
 }
---
+++ src/components/MainGrid/index.jsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const MainGrid = styled.main`
+  width: 100%;
+  grid-gap: 0.52vw;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 26.04vw;
+  padding: 0.83vw;
+  .profileArea {
+    display: none;
+    @media (min-width: 860px) {
+      display: block;
+    }
+  }
+  @media (min-width: 860px) {
+    max-width: 1110px;
+    display: grid;
+    grid-template-areas: "profileArea welcomeArea profileRelationsArea";
+    grid-template-columns: 8.33vw 1fr 16.25vw;
+  }
+`;
